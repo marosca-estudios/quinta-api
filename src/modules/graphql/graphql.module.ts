9701cda2624a4d40fb6cc68e1fffd94b844a02ed
andrefcasimiro <andrefcasimiro@gmail.com>
2020-04-09 21:36:46 +0100

    Updated graphql module

--- src/modules/graphql/graphql.module.ts
+++ src/modules/graphql/graphql.module.ts
@@ -5,7 +5,6 @@ import { UserModule } from 'src/data/modules/user.module'
 
 @Module({
   imports: [
-    UserModule,
     NestjsGraphQLModule.forRoot({
     installSubscriptionHandlers: true,
     autoSchemaFile: join(process.cwd(), 'src/schema.gql')
