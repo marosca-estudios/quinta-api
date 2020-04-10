ff7a83966737511fdf0e82da939798db529d0a49
André Fernandes <andre.fernandes@VAKT.io>
2020-04-10 19:28:21 +0100

    Adding context to provider

--- src/modules/user/user.provider.ts
+++ src/modules/user/user.provider.ts
@@ -1,17 +1,20 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Inject } from '@nestjs/common'
 import { Repository } from 'typeorm'
 import { User as UserEntity } from 'src/modules/user/user.entity'
 import { User as UserModel } from 'src/modules/user/user.model'
 import { InjectRepository } from '@nestjs/typeorm'
+import { CONTEXT } from '@nestjs/graphql'
 
 @Injectable()
 export class UserProvider {
   constructor(
-    @InjectRepository(UserEntity)
-    private usersRepository: Repository<UserModel>
+    @InjectRepository(UserEntity) private usersRepository: Repository<UserModel>,
+    @Inject(CONTEXT) private context,
   ){}
 
   async findById(id: number) {
+    console.log('received context: ', this.context)
+
     return this.usersRepository.findOne( { where: { id } })
   }
 
